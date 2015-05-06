21bf9a5b97974866c9d775a1af025e35715d64f4
Dinesh <dinesh.parasu@gmail.com>
2015-05-06 08:09:27 -0400

    Adding statuses updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campus::Application.config.secret_token = '7403a938ef470d790cc1ef7016daf9a516d3fd5c28dc86f7ce213623f8f552019a53b5665551ae8995e6aa5ee1f3c42c1199df597912f55d2c0c04a2033cf174'
