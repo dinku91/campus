4afaefc662520234ac478bdd49efb1bc09e4bc6f
Dinesh <dinesh.parasu@gmail.com>
2015-05-09 22:01:51 -0400

    Generated a profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Campus::Application.routes.draw do
+  get "profiles/show"
+
   devise_for :users
   
   devise_scope :user do
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -24,13 +24,5 @@ class UserTest < ActiveSupport::TestCase
 	assert !user.errors[:profile_name].empty?
 	end
 	
-	test "a user should have profile name without spaces" do
-	user = User.new
-	user.profile_name = "My Profile With spaces"
 	
-	assert !user.save
-	assert !user.errors[:profile_name].empty?
-	assert user.errors[:profile_name].include?("Must be formatted correctly.")
-	end
-
 end
